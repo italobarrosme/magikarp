d0b6692962e23f11a48bb989c17751a2dcca16a8
italobarrosme <italobarros.me@gmail.com>
2025-11-22 15:24:25 -0300

    chore: first commit install and config prisma and docker

--- prisma/lib/seed.ts
+++ prisma/lib/seed.ts
@@ -1,35 +1,29 @@
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
 async function main() {
-  await prisma.lead.createMany({
+  await prisma.user.createMany({
     data: [
       {
-        number: '11999999999',
-        name: 'John Doe',
-        profession: 'Developer',
-        age: 30,
-        gender: 'Male',
-        birthDate: new Date('1992-06-04'),
+        name: "John Doe",
+        phone: "11999999999",
+        role: "admin",
       },
       {
-        number: '11888888888',
-        name: 'Jane Smith',
-        profession: 'Designer',
-        age: 28,
-        gender: 'Female',
-        birthDate: new Date('1995-05-15'),
+        name: "Jane Smith",
+        phone: "11888888888",
+        role: "admin",
       },
     ],
-  })
+  });
 }
 
 main()
   .then(() => {
-    console.log('Seed completed!')
-    return prisma.$disconnect()
+    console.log("Seed completed!");
+    return prisma.$disconnect();
   })
   .catch((e) => {
-    console.error(e)
-    return prisma.$disconnect()
-  })
+    console.error(e);
+    return prisma.$disconnect();
+  });
