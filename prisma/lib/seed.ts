e36d3fd6c953ba2da710416780dcb969ab1b65e5
italobarrosme <italobarros.me@gmail.com>
2025-12-01 09:19:36 -0300

    refactor: update seed script for improved code consistency and formatting; standardize string quotes and enhance readability

--- prisma/lib/seed.ts
+++ prisma/lib/seed.ts
@@ -1,29 +1,27 @@
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
 async function main() {
   await prisma.user.createMany({
     data: [
       {
-        name: 'John Doe',
-        email: 'john.doe@example.com',
-        role: 'admin',
+        name: "John Doe",
+        email: "john.doe@example.com",
       },
       {
-        name: 'Jane Smith',
-        email: 'jane.smith@example.com',
-        role: 'admin',
+        name: "Jane Smith",
+        email: "jane.smith@example.com",
       },
     ],
-  })
+  });
 }
 
 main()
   .then(() => {
-    console.log('Seed completed!')
-    return prisma.$disconnect()
+    console.log("Seed completed!");
+    return prisma.$disconnect();
   })
   .catch((e) => {
-    console.error(e)
-    return prisma.$disconnect()
-  })
+    console.error(e);
+    return prisma.$disconnect();
+  });
